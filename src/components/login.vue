37513cb225b98056bda144d5c59f56c6d15fa60d
lihuan <15062006115@163.com>
2019-02-13 20:29:09 +0800

    登录完成

--- src/components/login.vue
+++ src/components/login.vue
@@ -15,49 +15,61 @@
 
 <script>
 export default {
-    data () {
-        return {
-            formData:{
-                username:'',
-                password:'',
-            }
-        }
-    },
-    methods:{
-        handleLogin(){
-            this.$http.post('login',this.formData)
-            .then(res=> {
-                // console.log(res)
-                const {data:{data,meta:{msg,status}}} = res
-                if(status === 200) {
-                    console.log(msg)
-                }else {
-                    this.$message.error(msg)
-                }
-                 
-            })
-        }
+  data() {
+    return {
+      formData: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+      // async,await简化代码，将异步操作看着像同步
+    async handleLogin() {
+      const res = await this.$http.post("login", this.formData)
+      const { data: { data:{token}, meta: { msg, status } } } = res
+      if (status === 200) {
+          // 永久保存数据‘token’
+        localStorage.setItem('token',token)
+        this.$router.push({
+            name:'home'
+        })
+      } else {
+          // 提示框组件
+        this.$message.error(msg)
+      }
     }
+  }
+};
 
-}
+// this.$http.post('login',this.formData)
+//             .then(res=> {
+//                 // console.log(res)
+//                 const {data:{data,meta:{msg,status}}} = res
+// if (status === 200) {
+//   console.log(msg);
+// } else {
+//   this.$message.error(msg);
+// }
+
+//             })
 </script>
   
 <style>
-  .login-wrap {
-      height: 100%;
-      background-color: #324152;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-  }
-  .login-form{
-      width: 400px;
-      background: #fff;
-      border-radius: 5px;
-      padding: 20px;
-
-  }
-  .login-btn{
-      width: 100%;
-  }
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  width: 400px;
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
