07554d781dc4314a9bfefb32d5ca84cf2d71adc8
lihuan <15062006115@163.com>
2019-02-15 16:20:00 +0800

    登录-侧边栏

--- src/components/home.vue
+++ src/components/home.vue
@@ -8,7 +8,80 @@
     </el-row>
   </el-header>
   <el-container>
-    <el-aside class="aside" width="200px">Aside</el-aside>
+    <el-aside class="aside" width="200px">
+     <el-menu
+      default-active="1" :router="true" :unique-opened="true">
+      <!-- 用户管理 -->
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+          <el-menu-item index="1-1">
+          <i class="el-icon-menu"></i>
+          用户列表            
+          </el-menu-item> 
+      </el-submenu>
+      <!-- 权限管理 -->
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template>
+          <el-menu-item index="1-1">
+            <i class="el-icon-menu"></i>
+            角色列表
+          </el-menu-item>
+          <el-menu-item index="1-1">
+            <i class="el-icon-menu"></i>
+            权限列表
+          </el-menu-item>  
+      </el-submenu> 
+      <!-- 商品管理 -->
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>商品管理</span>
+        </template>
+          <el-menu-item index="1-1">
+            <i class="el-icon-menu"></i>
+            商品列表
+          </el-menu-item>
+          <el-menu-item index="1-1">
+            <i class="el-icon-menu"></i>
+           分类参数
+          </el-menu-item> 
+          <el-menu-item index="1-1">
+            <i class="el-icon-menu"></i>
+            商品分类
+          </el-menu-item>  
+      </el-submenu>
+      <!-- 订单管理  -->
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>订单管理</span>
+        </template>
+          <el-menu-item index="1-1">
+             <i class="el-icon-menu"></i>
+             订单列表
+          </el-menu-item> 
+      </el-submenu> 
+      <!-- 数据统计 -->
+      <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>数据统计</span>
+        </template>
+          <el-menu-item index="1-1"> 
+           <i class="el-icon-location"></i>
+           数据报表
+          </el-menu-item> 
+      </el-submenu> 
+       
+       
+    </el-menu>
+    </el-aside>
     <el-main class="main">Main</el-main>
   </el-container>
  </el-container>
@@ -24,17 +97,18 @@ export default {};
   height: 100%;
 }
 .aside {
-  background-color: #ccc;
+  /* background-color: #ccc; */
+  height: 100%;
 }
 .main {
   background-color: #333;
 }
-.middle{
-    line-height: 60px;
-    text-align: center;
+.middle {
+  line-height: 60px;
+  text-align: center;
 }
-.logout{
-    line-height: 60px;
-    text-decoration: none;
+.logout {
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
